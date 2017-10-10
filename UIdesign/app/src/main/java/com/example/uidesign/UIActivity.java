5ee0bbf41eb8dcf8556651d791f6217551ac3038
Kangbeibei <462361315@qq.com>
2017-10-10 13:01:47 +0800

    create UIdesign

---
+++ UIdesign/app/src/main/java/com/example/uidesign/UIActivity.java
@@ -0,0 +1,12 @@
+package com.example.uidesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
